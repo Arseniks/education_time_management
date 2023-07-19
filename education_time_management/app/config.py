350a04a7d4a0f51bd32f7e4ed4f0fb7ec1222d8e
Arseniks <senya.korotkov@gmail.com>
2023-07-19 13:08:35 +0300

    Adding .env.template and small refactoring

--- education_time_management/app/config.py
+++ education_time_management/app/config.py
@@ -9,3 +9,4 @@ DB_CONNECTION_URI = os.environ.get("DB_CONNECTION_URI", "postgresql://postgres:p
 BACKEND_URL = os.environ.get("BACKEND_URL", "http://localhost")
 BACKEND_PORT = os.environ.get("BACKEND_PORT", "5001")
 BACKEND_URL_WITH_PORT = f"{BACKEND_URL}:{BACKEND_PORT}"
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "SECRET")
